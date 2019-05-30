3ec3b32412806ee0de9b60b05314247efd5ebb8e
Lúcás Meier <cronokirby@gmail.com>
2019-05-30 21:49:40 +0200

    Add function to list all files in a directory

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
+// dirFileNames returns a list of file names in a given directory
+// errors can happen due to IO, or if the given directory doesn't exist
+func dirFileNames(dir string) ([]string, error) {
+	file, err := os.Open(dir)
+	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
+	names, err := file.Readdirnames(0)
+	if err != nil {
+		return nil, err
+	}
+	return names, nil
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	args := os.Args[1:]
+	dir := args[0]
+	fileNames, err := dirFileNames(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(fileNames)
 }
